eaa0dd2ebd4f0e0e16f0c3a54ece07fd669693b6
yakirp <yakirperlin@gmail.com>
2016-09-29 18:22:04 +0300

    update <dependency>dependency

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -17,7 +17,7 @@ public class Main extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Main() {
+    public Main() {  
         super();
         // TODO Auto-generated constructor stub
     }  
@@ -27,7 +27,7 @@ public class Main extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at : ").append(request.getContextPath());
+		response.getWriter().append("Servdfdved at : ").append(request.getContextPath());
 	}
 
 	/**
